ebee24cfb542f4d5be52ef587f0fd4871937d47c
Michael Garcia dev <{ID}+{username}@users.noreply.github.com>
2024-10-07 12:18:30 -0700

    Finish tests / associated functionality

--- src/capitalize.js
+++ src/capitalize.js
@@ -1,14 +1,14 @@
 export default function capitalize(str) {
+  if (typeof str === "string") {
     const firstChar = str.slice(0, 1);
+    const remainingStr = str.slice(1, str.length);
 
-    if (firstChar) {
-        const remainingStr = str.slice(1, str.length);
-        let newStr = "";
+    let newStr = "";
 
-        newStr = firstChar.toUpperCase() + remainingStr;
-    
-        return newStr;
-    } else {
-        return null;
-    }
+    newStr = firstChar.toUpperCase() + remainingStr;
+
+    return newStr;
+  } else {
+    return null;
+  }
 }
--- tests/capitalize.test.js
+++ tests/capitalize.test.js
@@ -1,10 +1,29 @@
 import capitalize from "../src/capitalize.js";
 
 test("capitalizes first character", () => {
-    expect(capitalize("hello")).toBe("Hello");
+  expect(capitalize("hello")).toBe("Hello");
 });
 
-test("rejects empty string", () => {
-    expect(capitalize("")).toBeNull();
+test("handles empty string", () => {
+  expect(capitalize("")).toBe("");
 });
 
+test("handles whitespace", () => {
+  expect(capitalize("  ")).toBe("  ");
+});
+
+test("capitalizes a single char string", () => {
+  expect(capitalize("h")).toBe("H");
+});
+
+test("do nothing to all caps string", () => {
+  expect(capitalize("HELLO")).toBe("HELLO");
+});
+
+test("handle other types", () => {
+  expect(capitalize(34535435)).toBeNull();
+});
+
+test("handle weird string", () => {
+  expect(capitalize("^Y*$^@66ffhd**^DHI")).toBe("^Y*$^@66ffhd**^DHI");
+});
